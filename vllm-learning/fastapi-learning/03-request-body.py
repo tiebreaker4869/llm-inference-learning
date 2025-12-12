e58213c6ac4bda1ab8c70e67660186240b05ba57
Lifan Sun <lifansun1412@gmail.com>
2025-12-12 11:12:59 -0800

    feat: fastapi step 03 request body

---
+++ vllm-learning/fastapi-learning/03-request-body.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: str | None
+    price: float
+    tax: float | None
+    
+@app.post("/items")
+async def create_item(item: Item):
+    return {
+        "name": item.name,
+        "description": item.description,
+        "price": item.price,
+        "tax": item.tax
+    }
