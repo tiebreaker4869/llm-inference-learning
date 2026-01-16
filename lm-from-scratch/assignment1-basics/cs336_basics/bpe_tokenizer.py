3e6e41c344cdeb8cc1dada30647ff616ea39a45b
Lifan Sun <lifansun1412@gmail.com>
2026-01-16 14:37:59 -0800

    fix: fix decode error handling

--- lm-from-scratch/assignment1-basics/cs336_basics/bpe_tokenizer.py
+++ lm-from-scratch/assignment1-basics/cs336_basics/bpe_tokenizer.py
@@ -13,22 +13,22 @@ class BPETokenizer(Tokenizer):
     def __init__(self, params: BPETokenizerParams):
         self.params = params
         self.bytes_to_token = {bs : idx for idx, bs in self.params.vocab.items()}
-        self.special_tokens_regex = re.escape("|").join(self.params.special_tokens) if self.params.special_tokens else ""
         self.special_token_to_idx = dict()
+        self.special_tokens_regex = None
         if self.params.special_tokens:
             self._init_special_tokens()
         self._init_merges()
         
     def _init_merges(self):
-        self.merges_idx: dict[tuple[int, int], int] = dict()
+        self.merges_idx: list[tuple[tuple[int, int], int]] = []
         for b1, b2 in self.params.merges:
             idx1, idx2 = self.bytes_to_token[b1], self.bytes_to_token[b2]
             merge_idx = self.bytes_to_token[b1 + b2]
-            self.merges_idx[(idx1, idx2)] = merge_idx
-            
-        
+            self.merges_idx.append(((idx1, idx2), merge_idx))
 
     def _init_special_tokens(self):
+        escaped_tokens = [re.escape(st) for st in self.params.special_tokens]
+        self.special_tokens_regex = "(" + "|".join(escaped_tokens) + ")"
         special_tokens_set = set(self.params.special_tokens)
         special_tokens_set = set({t.encode("utf-8") for t in special_tokens_set})
         for token, bstring in self.params.vocab.items():
@@ -37,25 +37,28 @@ class BPETokenizer(Tokenizer):
                 self.special_token_to_idx[string] = token
         
     def encode(self, string: str) -> list[int]:
-        pretokens = pretokenize(string)
+        splitted = re.split(self.special_tokens_regex, string) if self.params.special_tokens else [string]
         indices = []
-        for pretoken in pretokens:
-            parts = re.split(self.special_tokens_regex, pretoken) if self.params.special_tokens else [pretoken]
-            print(list(parts))
-            for part in parts:
-                if part in self.special_token_to_idx:
-                    indices.append(self.special_token_to_idx[part])
-                else:
-                    bs = part.encode("utf-8")
+        for chunk in splitted:
+            if not chunk:
+                continue
+            if chunk in self.special_token_to_idx:
+                indices.append(self.special_token_to_idx[chunk])
+            else:
+                pretokens = pretokenize(chunk)
+                for pretoken in pretokens:
+                    if not pretoken:
+                        continue
+                    bs = pretoken.encode("utf-8")
                     bs_list = [bs[i:i+1] for i in range(len(bs))]
                     idxs = [self.bytes_to_token[b] for b in bs_list]
-                    for pair, new_idx in self.merges_idx.items():
+                    for pair, new_idx in self.merges_idx:
                         idxs = self._merge(idxs, pair, new_idx)
                     indices.extend(idxs)
         return indices
     def decode(self, tokens: list[int]) -> str:
-        bytes_list = [self.params.vocab.get(token) for token in tokens]
-        string = b"".join(bytes_list).decode("utf-8")
+        bytes_list = [self.params.vocab[token] for token in tokens]
+        string = b"".join(bytes_list).decode("utf-8", errors='replace')
         return string
     
     @staticmethod
