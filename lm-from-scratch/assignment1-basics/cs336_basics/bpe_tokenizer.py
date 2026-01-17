3e55ae5ddeb3f2fe4f5847fb1146a2e06957a469
Lifan Sun <lifansun1412@gmail.com>
2026-01-16 16:05:13 -0800

    fix: add complete interface for tokenizer

--- lm-from-scratch/assignment1-basics/cs336_basics/bpe_tokenizer.py
+++ lm-from-scratch/assignment1-basics/cs336_basics/bpe_tokenizer.py
@@ -1,6 +1,7 @@
 from .tokenizer import Tokenizer, pretokenize
 from dataclasses import dataclass
 import regex as re
+from typing import Iterable, Iterator
 
 @dataclass(frozen=True)
 class BPETokenizerParams:
@@ -11,34 +12,50 @@ class BPETokenizerParams:
     
 class BPETokenizer(Tokenizer):
     def __init__(self, params: BPETokenizerParams):
-        self.params = params
-        self.bytes_to_token = {bs : idx for idx, bs in self.params.vocab.items()}
+        self.vocab = params.vocab
+        self.special_tokens = params.special_tokens
         self.special_token_to_idx = dict()
         self.special_tokens_regex = None
-        if self.params.special_tokens:
+        if self.special_tokens:
             self._init_special_tokens()
-        self._init_merges()
+        self.bytes_to_idx = {bs : idx for idx, bs in self.vocab.items()}
+        self._init_merges(params.merges)
         
-    def _init_merges(self):
+    def _init_merges(self, merges: list[tuple[bytes, bytes]]):
         self.merges_idx: list[tuple[tuple[int, int], int]] = []
-        for b1, b2 in self.params.merges:
-            idx1, idx2 = self.bytes_to_token[b1], self.bytes_to_token[b2]
-            merge_idx = self.bytes_to_token[b1 + b2]
+        for b1, b2 in merges:
+            idx1, idx2 = self.bytes_to_idx[b1], self.bytes_to_idx[b2]
+            merge_idx = self.bytes_to_idx[b1 + b2]
             self.merges_idx.append(((idx1, idx2), merge_idx))
 
     def _init_special_tokens(self):
-        escaped_tokens = [re.escape(st) for st in self.params.special_tokens]
+        # initialize special token regex
+        escaped_tokens = [re.escape(st) for st in self.special_tokens]
         escaped_tokens.sort(key = len, reverse = True)
         self.special_tokens_regex = "(" + "|".join(escaped_tokens) + ")"
-        special_tokens_set = set(self.params.special_tokens)
-        special_tokens_set = set({t.encode("utf-8") for t in special_tokens_set})
-        for token, bstring in self.params.vocab.items():
-            if bstring in special_tokens_set:
+        
+        # add new special tokens
+        special_tokens_set = set(self.special_tokens)
+        bytes_special_tokens_set = set({s.encode("utf-8") for s in special_tokens_set})
+        in_vocab_special_tokens = set({t for t in self.vocab.values()if t in bytes_special_tokens_set})
+        in_vocab_special_tokens = set({bs.decode("utf-8") for bs in in_vocab_special_tokens})
+        nxt_idx = max(self.vocab.keys()) + 1
+        new_special_tokens = special_tokens_set - in_vocab_special_tokens
+        for new_special_token in new_special_tokens:
+            self.vocab[nxt_idx] = new_special_token.encode("utf-8")
+            nxt_idx += 1
+        
+        # add reverse mapping for special tokens
+        for token, bstring in self.vocab.items():
+            if bstring in bytes_special_tokens_set:
                 string = bstring.decode("utf-8")
                 self.special_token_to_idx[string] = token
+
+    def from_files(cls, vocab_filepath: str, merges_filepath: str, special_tokens: list[str] | None = None):
+        pass
         
     def encode(self, string: str) -> list[int]:
-        splitted = re.split(self.special_tokens_regex, string) if self.params.special_tokens else [string]
+        splitted = re.split(self.special_tokens_regex, string) if self.special_tokens else [string]
         indices = []
         for chunk in splitted:
             if not chunk:
@@ -52,15 +69,21 @@ class BPETokenizer(Tokenizer):
                         continue
                     bs = pretoken.encode("utf-8")
                     bs_list = [bs[i:i+1] for i in range(len(bs))]
-                    idxs = [self.bytes_to_token[b] for b in bs_list]
+                    idxs = [self.bytes_to_idx[b] for b in bs_list]
                     for pair, new_idx in self.merges_idx:
                         idxs = self._merge(idxs, pair, new_idx)
                     indices.extend(idxs)
         return indices
     def decode(self, tokens: list[int]) -> str:
-        bytes_list = [self.params.vocab[token] for token in tokens]
+        bytes_list = [self.vocab[token] for token in tokens]
         string = b"".join(bytes_list).decode("utf-8", errors='replace')
         return string
+    
+    def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
+        for string in iterable:
+            tokens = self.encode(string)
+            for token in tokens:
+                yield token
     
     @staticmethod
     def _merge(indices: list[int], pair: tuple[int, int], new_index: int) -> list[int]:
